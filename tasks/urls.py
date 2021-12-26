86dfecf2ee902496025e41eb4251a45d7c788b92
Heitor Augusto <heitor.a.ha@gmail.com>
2021-12-26 19:22:56 -0300

    Adicionando funcionalidade de Filtro por categoria

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import home, tarefa, nova_tarefa, delete
+from .views import home, tarefa, nova_tarefa, delete, filtrar
 
 urlpatterns = [
     path('', home, name='home'),
     path('tarefa/<int:pk>', tarefa, name='tarefa'),
     path('delete/<int:pk>', delete, name='delete'),
+    path('filtro/<str:pk>', filtrar, name='filtro'),
     path('nova', nova_tarefa, name='nova'),
 ]
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render, redirect
 from .form import TarefaForm
-from .models import Tarefa
+from .models import Tarefa, Categoria
 import datetime
 # Create your views here. Funções que serão chamadas nas rotas
 
 
 def home(request):
     tarefas = Tarefa.objects.all()
+    categorias = Categoria.objects.all()
 
     context = {
         'tarefas': tarefas,
+        'categoria': categorias,
     }
 
     return render(request, 'home.html', context)
+
+
+def filtrar(request, pk):
+    filtros = Tarefa.objects.filter(categoria=pk)
+    categorias = Categoria.objects.all()
+
+    context = {
+        'filtre': filtros,
+        'categoria': categorias,
+    }
+
+    return render(request, 'filtro.html', context)
 
 
 def tarefa(request, pk):
