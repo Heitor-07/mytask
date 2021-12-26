0213ad8ef1f98d34bcaac5b03ea6762dfecb7830
Heitor Augusto <heitor.a.ha@gmail.com>
2021-12-26 01:01:44 -0300

    Separando e criando models Categoria

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,15 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Tarefa, Usuario
+from .models import Tarefa, Usuario, Categoria
 
 
 class TarefaAdmin(admin.ModelAdmin):
-    list_display = ('nome_tarefa', 'dia', 'descricao')
+    list_display = ('nome_tarefa', 'dia', 'descricao', 'categoria')
 
 
+admin.site.register(Categoria)
 admin.site.register(Tarefa, TarefaAdmin)
 admin.site.register(Usuario)
+
 
 
 
--- tasks/models.py
+++ tasks/models.py
@@ -5,12 +5,18 @@ from django.db import models
 # Create your models here. Classes que vão para o banco de dados
 
 
+class Categoria(models.Model):
+    categoria = models.CharField('Categoria', max_length=50)
+
+    def __str__(self):
+        return self.categoria
+
+
 class Tarefa(models.Model):
     nome_tarefa = models.CharField('Tarefa', max_length=100)
     dia = models.DateField('Dia', max_length=11)
     descricao = models.CharField('Descrição', max_length=500)
-    categoria = models.CharField('Categoria', max_length=30)
-
+    categoria = models.ForeignKey(Categoria, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome_tarefa
